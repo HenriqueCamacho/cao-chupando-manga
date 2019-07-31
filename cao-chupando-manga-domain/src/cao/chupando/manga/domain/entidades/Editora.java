cf4c805fb105bbf39c4fcd543d98da257864425e
HenriqueCamacho <harrycamachofarias@hotmail.com>
2019-07-31 19:53:35 -0400

    Adiciona a entidadade Editora

---
+++ cao-chupando-manga-domain/src/cao/chupando/manga/domain/entidades/Editora.java
@@ -0,0 +1,14 @@
+
+package cao.chupando.manga.domain.entidades;
+
+/**
+ *
+ * @author henrique
+ */
+public class Editora {
+    private int id;
+    private String nome;
+    private String nacionalidade;
+    
+    
+}
